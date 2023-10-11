198b9ee2f914bbf650f29176663c7454e14c596b
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-10-11 15:17:38 +0100

    Add JS code for pwd verification
    Want to get this reviewed to confirm if I could have done
    something better (especially about the positioning);

---
+++ script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("button");
+const pwd = document.querySelector("#pwd");
+const pwdConfirmation = document.querySelector("#pwd-confirmation");
+const form = document.querySelector("form");
+const errorText = document.createElement("p");
+
+errorText.textContent = "*Passwords do not match";
+errorText.classList.add("errorText");
+
+button.addEventListener("click", (e) => {
+    if (pwd.value !== pwdConfirmation.value){
+        pwd.classList.add("error");
+        pwdConfirmation.classList.add("error");
+        form.appendChild(errorText);
+    }
+    else {
+        pwd.classList.remove("error");
+        pwdConfirmation.classList.remove("error");
+        form.removeChild(errorText);
+    }
+})
